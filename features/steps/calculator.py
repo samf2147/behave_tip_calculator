d9f52eb6db9d089b3dca2a1fbabcb0f357cdbf6b
Sam Frank <sfrank2147@gmail.com>
2014-01-11 19:06:39 -0600

    passed selenium test

---
+++ features/steps/calculator.py
@@ -0,0 +1,9 @@
+from flask import url_for
+
+@when(u'I go to the tip calculator')
+def step_impl(context):
+    context.browser.get('http://localhost:5000')
+
+@then(u'I should see the calculator form')
+def step_impl(context):
+    assert context.browser.title == 'Tip calculator'
