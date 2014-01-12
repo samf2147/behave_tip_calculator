a3e8c8ff255aba8e06e620f9ad7392b210f13f41
Sam Frank <sfrank2147@gmail.com>
2014-01-11 18:54:37 -0600

    removed google tests

--- features/steps/google.py
+++
@@ -1,9 +0,0 @@
-@when(u'I go to the google home page')
-def step_impl(context):
-    '''have selenium navigate to google's home page'''
-    context.browser.get('http://www.google.com')
-
-@then(u'I should see that the title is "Google"')
-def step_impl(context):
-    '''check that title of most recently visited page is Google'''
-    assert context.browser.title == "Google"
